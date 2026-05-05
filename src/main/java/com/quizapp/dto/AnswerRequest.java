dfedba141cc13c3102dcf7e8ca85a68f9147bbd5
Shreya3884 <sj0711778@gmail.com>
2026-05-05 15:26:55 +0530

    Add AnswerRequest DTO

--- src/main/java/com/quizapp/dto/AnswerRequest.java
+++ src/main/java/com/quizapp/dto/AnswerRequest.java
@@ -1,5 +1,8 @@
 package com.quizapp.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,8 +13,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class AnswerRequest {
+    @NotNull(message = "Student database id is required")
     private Long studentId;
+
+    @NotNull(message = "Question id is required")
     private Long questionId;
+
+    @NotNull(message = "Selected option index is required")
+    @Min(value = 0, message = "Selected option index must be zero or greater")
     private Integer selectedOption;
+
+    @NotBlank(message = "Session code is required")
     private String sessionCode;
 }
